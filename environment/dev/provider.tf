53ec0a110c189e0e35f3084e8245fd050d2149ec
lavjeet223 <lavjeet22@gmail.com>
2025-11-26 14:16:32 +0530

    backed added in Teraform block

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -5,7 +5,13 @@ terraform {
       version = "4.0.1"
     }
   }
-}
+   backend "azurerm" {
+    resource_group_name  = "Backend"
+    storage_account_name = "backendstg112233"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+
+
 
 provider "azurerm" {
   features {}

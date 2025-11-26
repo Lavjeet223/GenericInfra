82875fb2d185337eb685414ea5fac93f66ce13df
lavjeet223 <lavjeet22@gmail.com>
2025-11-26 14:04:39 +0530

    Storage account name added container block instead of id

--- backend/main.tf
+++ backend/main.tf
@@ -13,23 +13,23 @@ subscription_id= "b0805f22-5464-4af2-968e-fabac43ebd20"
 }
 
 
-resource "azurerm_resource_group" "lavbackendrgblock" {
+resource "azurerm_resource_group" "rgbackend" {
   name     = "Backend"
   location = "centralindia"
 
 }
 
-resource "azurerm_storage_account" "lavbackendstgblock" {
+resource "azurerm_storage_account" "stgbackend" {
   name                     = "backendstg112233"
-  resource_group_name      = azurerm_resource_group.lavbackendrgblock.name
+  resource_group_name      = azurerm_resource_group.rgbackend.name
   location                 = "centralindia"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "lavbackendcontblock" {
+resource "azurerm_storage_container" "contbackend" {
   name                  = "tfstate"
-  storage_account_id    = azurerm_storage_account.lavbackendstgblock.id
+  storage_account_name    = azurerm_storage_account.stgbackend.name
   container_access_type = "private"
 
 }
